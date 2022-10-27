f0841f0dc2cec6307f90a01723d6b989989bd19f
Sandro <sandro2007am@gmail.com>
2022-10-27 11:52:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-CHAVE = 13
-
-def cipher(message, dir):
+def cifra(mensagem, dir):
+    #variavel com mensagem cifrada
     m = ''
-    for c in message:
-        if c in ALPHABET:
-            c_index = ALPHABET.index(c)
-            m += ALPHABET[(c_index + (dir * ROT)) % len(ALPHABET)]
+    #Percorre a mensagem e verifica os caracteres
+    for c in mensagem:
+        #caso mensagem contenha caracteres, armazena o índice do caracter na string
+        if c in ALFABETO:
+            c_index = ALFABETO.index(c)
+            #Dependendo da direção, a mensagem será criptografada (1) ou descriptografada (-1)
+            m += ALFABETO[(c_index + (dir * CHAVE)) % len(ALFABETO)]
         else:
+            #caso o caractere não esteja no alfabeto, será armazenado sem alteração
             m += c
     return m
 
-def encrypt(message):
-    return cipher(message, 1)
+def encrypt(mensagem):
+    return cifra(mensagem, 1)
 
-def decrypt(message):
-    return cipher(message, -1)
+def decrypt(mensagem):
+    return cifra(mensagem, -1)
 
 def main():
-    command = sys.argv[1].lower()
-    message = sys.argv[2].lower()
-
-    if command == 'encrypt':
-        print encrypt(message)
-    elif command == 'decrypt':
-        print decrypt(message)
-    else:
-        print command + ' -> command not found'
-
+    ALFABETO = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    CHAVE = 13
+    original = 'a ligeira raposa marrom saltou sobre o cachorro cansado'
+    print('  Original:', original)
+    cifrada = encrypt(original, 1)
+    print('Encriptada:', ciphered)
+    plain = decrypt(cifrada, -1)
+    print('Decriptada:', plain)
+    
 if __name__ == '__main__':
     main()
